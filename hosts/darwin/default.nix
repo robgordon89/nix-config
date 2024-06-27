ebb52fcc4005ebe0032870602a4ecd4ba9ddcabe
Robert Gordon <rob@ruled.io>
2024-06-27 19:17:55 +0100

    chore: remove extras set already in sre flake

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -5,16 +5,4 @@
     # Custom Packages
     # bob
   ];
-
-  # Enable the nix daemon
-  services.nix-daemon.enable = true;
-
-  # Enable not free packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable experimental features
-  nix.settings.experimental-features = "nix-command flakes";
-
-  # Set the host platform to aarch64-darwin
-  nixpkgs.hostPlatform = "aarch64-darwin";
 }
