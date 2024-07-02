4aa49e571bb2995eff79d8ff1b6ee875eef88b66
Robert Gordon <rob@ruled.io>
2024-07-02 16:58:18 +0100

    chore: setup some sane defaults finder

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -4,5 +4,20 @@ let
   user = "robert";
 in
 {
+     # Disable man pages
      documentation.man.enable = false;
+
+     # Set some snae defaults
+     system.defaults = {
+
+        # Set some finder defaults
+        finder = {
+            ShowPathbar = true;
+            FXEnableExtensionChangeWarning = false;
+            ShowStatusBar = false;
+        };
+     };
+    
+    # Allow the user to use sudo with Touch ID
+    security.pam.enableSudoTouchIdAuth = true;
 }
