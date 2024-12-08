2de0ae49406f28119b14809af26d335c4b0c6f89
Robert Gordon <rob@ruled.io>
2024-12-08 20:37:29 +0000

    chore: update home

--- hosts/common/core/fonts.nix
+++ hosts/common/core/fonts.nix
@@ -6,12 +6,8 @@
 
 {
   fonts.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "DroidSansMono"
-      ];
-    })
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
     geist-font
   ];
 }
--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -13,15 +13,14 @@
     # Making legacy nix commands consistent as well, awesome!
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
+    optimise.automatic = true;
+
     settings = {
       # See https://jackson.dev/post/nix-reasonable-defaults/
       connect-timeout = 5;
       log-lines = 25;
       min-free = 128000000; # 128MB
       max-free = 1000000000; # 1GB
-
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
 
       # experimental-features = [
       #   "nix-command"
