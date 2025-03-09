9ad209d3f83de713f28ba4bc429df8a34b430f01
Robert Gordon <rob@ruled.io>
2025-03-09 13:55:46 +0000

    chore: updates

--- modules/shared/home.nix
+++ modules/shared/home.nix
@@ -1,13 +1,14 @@
 { ... }: {
   imports = [
-    ./programs/bat
     ./programs/git
     ./programs/neovim
-    ./programs/tmux
     ./programs/zsh
+    ./programs/bat.nix
     ./programs/direnv.nix
     ./programs/fd.nix
     ./programs/k9s.nix
+    ./programs/lazygit.nix
+    ./programs/tmux.nix
     ./programs/zoxide.nix
   ];
 }
