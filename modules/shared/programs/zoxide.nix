37e0b65a2102ad6d03ce0c57b25f64fef00b7a66
Robert Gordon <rob@ruled.io>
2025-12-29 17:58:40 +0000

    chore: fix for direnv and zoxide

--- modules/shared/programs/zoxide.nix
+++ modules/shared/programs/zoxide.nix
@@ -2,7 +2,6 @@
 {
   programs.zoxide = {
     enable = true;
-    enableBashIntegration = true;
     enableZshIntegration = true;
     options = [
       "--cmd cd" # replace cd with z and zi (via cdi)
