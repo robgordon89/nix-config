8ba363c0c7221d8cba143941a37768960567b2a6
Robert Gordon <rob@ruled.io>
2026-02-27 09:51:49 +0000

    add test host

--- hosts.nix
+++ hosts.nix
@@ -52,4 +52,41 @@
     extraDarwinModules = [ ];
     extraHomeManagerModules = [ ];
   };
+  ManagedsVirtualMachine = {
+    extraConfig = {
+      username = "admin"
+        dockPathOverrides = {
+      # Use Slack at work
+      "/Applications/Beeper Desktop.app/" = "/Applications/Slack.app/";
+    };
+    extraHomeManagerPackages = [ ]
+      ++ mailerlite.pkgs.aarch64-darwin.sre;
+    claudeCode = {
+      useVertex = true;
+      vertexProjectId = "mailerlite-claude-code";
+    };
+  };
+  extraDarwinModules = [
+    mailerlite.modules.darwin.defaults
+    {
+      mailerlite = {
+        team = "sre";
+      };
+    }
+  ];
+  extraHomeManagerModules = [
+    mailerlite.modules.home-manager.defaults
+    {
+      mailerlite =
+        {
+          team = "sre";
+          # Disable modules that I don't use
+          direnv.enable = false; # I use my own module
+          ssh = {
+            username = "admin";
+          };
+        };
+    }
+  ];
+};
 }
