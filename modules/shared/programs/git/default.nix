dc184810ecde0b87a3cf17d2884611e494e5791d
Robert Gordon <rob@ruled.io>
2026-04-09 12:00:01 +0100

    fixes

--- modules/shared/programs/git/default.nix
+++ modules/shared/programs/git/default.nix
@@ -8,14 +8,12 @@
       signByDefault = true;
     };
     ignores = import ./config/ignores.nix;
-    settings =
-      import ./config/extra.nix
-      // {
-        user = {
-          name = hostConfig.fullName;
-          email = hostConfig.email;
-        };
-        alias = import ./config/aliases.nix;
+    settings = import ./config/extra.nix // {
+      user = {
+        name = hostConfig.fullName;
+        email = hostConfig.email;
       };
+      alias = import ./config/aliases.nix;
+    };
   };
 }
