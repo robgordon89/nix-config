6030cb9b73e10131cb900d2eb77f057bad7433d9
Robert Gordon <rob@ruled.io>
2026-06-17 12:21:07 +0100

    add ssh launchd agent to user

--- modules/features/developer/1password/1password.nix
+++ modules/features/developer/1password/1password.nix
@@ -1,25 +1,26 @@
 { ... }:
 {
-  flake.modules.darwin.onePassword = { config, ... }: {
+  flake.modules.darwin.onePassword = { ... }: {
     homebrew.casks = [{ name = "1password"; greedy = true; }];
+  };
 
-    launchd.user.agents."com.1password.SSH_AUTH_SOCK" = {
-      serviceConfig = {
+  flake.modules.homeManager.onePassword = { config, ... }: {
+    home.file.".config/1Password/ssh/agent.toml" = {
+      source = ./_1password-agent.toml;
+      recursive = true;
+    };
+
+    launchd.agents."com.1password.SSH_AUTH_SOCK" = {
+      enable = true;
+      config = {
         Label = "com.1password.SSH_AUTH_SOCK";
         ProgramArguments = [
           "/bin/sh"
           "-c"
-          "/bin/ln -sf /Users/${config.meta.username}/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock $SSH_AUTH_SOCK"
+          "/bin/ln -sf ${config.home.homeDirectory}/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock $SSH_AUTH_SOCK"
         ];
         RunAtLoad = true;
       };
-    };
-  };
-
-  flake.modules.homeManager.onePassword = { ... }: {
-    home.file.".config/1Password/ssh/agent.toml" = {
-      source = ./_1password-agent.toml;
-      recursive = true;
     };
   };
 }
